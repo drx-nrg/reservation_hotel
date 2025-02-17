be9f136d540c5e58c461f9fd63f50a7362dad674
FanzilFaiz <faiz.maula08@gmail.com>
2025-02-17 07:51:30 +0700

    faiz

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -17,7 +17,16 @@ class RoomController extends Controller
     }
 
     public function store(Request $request) {
-        //
+        $request->validate([
+            'room_number' => 'required|unique:rooms|max:10',
+            'room_type' => 'required',
+            'price' => 'required|numeric',
+            'facilities' => 'required',
+            'status' => 'required'    
+        ]);
+        Room::create($request->all());
+        return redirect()->route('rooms.index')->with('success', 'Room created successfully.');
+
     }
 
     public function edit(Room $room) {
@@ -25,7 +34,16 @@ class RoomController extends Controller
     }
 
     public function update(Request $request, Room $room) {
-        // 
+        $request->validate([
+            'room_number' => 'required|max:10|unique:rooms,room_number,' . $room->id,
+            'room_type' => 'required',
+            'price_per_night' => 'required|numeric',
+            'facilities' => 'required',
+            'status' => 'required'
+        ]);
+
+        $room->update($request->all());
+        return redirect()->route('rooms.index')->with('success', 'Room updated successfully.');
     }
 
     public function destroy(Room $room) {
